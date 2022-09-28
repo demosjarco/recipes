b631de9228e531aa3d8a1279dc4e8424f742b2ff
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2022-09-28 15:57:54 -0700

    Use python `lesscpy` instead of node `less-watch-compiler`

--- hooks/lessCompiler.py
+++ hooks/lessCompiler.py
@@ -1,3 +1,20 @@
+from pathlib import Path
+from io import StringIO
+import lesscpy
+
+class LessCompiler:
+	def compileCSS(cssFilePath:Path, lessFilePath:Path) -> None:
+		with open(lessFilePath, 'r') as lessFile:
+			lessFileText = lessFile.read()
+		
+		cssFileText = lesscpy.compile(StringIO(lessFileText), tabs=True, spaces=False)
+		with open(cssFilePath, 'w') as cssFile:
+			cssFile.write(cssFileText)
+
 # Run `on_config` because it runs before `get_files`
 def on_config(config):
-	pass
+	for extraCssFilePath in config.extra_css:
+		cssFilePath = Path("recipes").joinpath(Path(extraCssFilePath))
+		lessFilePath = cssFilePath.with_suffix('.less')
+
+		LessCompiler.compileCSS(cssFilePath, lessFilePath)
