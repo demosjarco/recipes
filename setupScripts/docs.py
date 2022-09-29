3c63ba4424b8bf2fdf5a39c87e3b298c84420a06
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2022-09-28 22:54:57 -0700

    Moved to hooks

--- setupScripts/docs.py
+++ setupScripts/docs.py
@@ -1,30 +1,15 @@
 import os
 import subprocess
 from .ciSystem import CiSystem
-from .preSetup import PreSetup
 from .cookLang import CookLang
 from .cookDocs import CookDocs
 import shutil
 
 class Docs:
 	def __init__(self) -> None:
-		self.systemType = self.setupEnv()
 		# self.systemDebug()
 		CookLang()
 		CookDocs()
-	
-	def setupEnv(self) -> CiSystem:
-		if (os.getenv('CI_SYSTEM_OVERRIDE') != None and int(os.getenv('CI_SYSTEM_OVERRIDE')) >= 0):
-			return CiSystem(int(os.getenv('CI_SYSTEM_OVERRIDE')))
-		else:
-			if (os.getenv('CF_PAGES') != None and int(os.getenv('CF_PAGES')) == 1):
-				# Install MkDocs dependencies
-				PreSetup().cf.installMkDocsDeps()
-				# Install node packages since CF only installs python packages by default
-				PreSetup().cf.npmCi()
-				return CiSystem.CLOUDFLARE
-			elif (os.getenv('GITHUB_ACTIONS') != None and bool(os.getenv('GITHUB_ACTIONS')) == True):
-				return CiSystem.GITHUB
 	
 	def systemDebug(self) -> None:
 		print('Detected CI system', self.systemType, flush=True)
