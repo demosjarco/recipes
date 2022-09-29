c904cf78af2cc3b7fe56379561d07a59b1057ca0
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2022-09-28 22:52:26 -0700

    Setup social deps

---
+++ hooks/preSetup.py
@@ -0,0 +1,21 @@
+from sys import platform
+from os import system, getenv
+
+class AptInstall:
+	def __init__(self) -> None:
+		system(f'{self.isSudo() == True and "" or "sudo"} apt update')
+
+	def isSudo(self) -> bool:
+		if not os.environ.get("SUDO_UID") and os.geteuid() != 0:
+			return False
+		else:
+			return True
+
+	def installPackages(self, *packages:str, accept:bool = False) -> None:
+		system(f'{self.isSudo() == True and "" or "sudo"} apt install {accept == True and "-y" or ""} '.join(packages))
+
+def on_startup(command, dirty:bool):
+	if (getenv('ENABLED_SOCIAL') != None and bool(getenv('ENABLED_SOCIAL'))):
+		# MkDocs social requirement
+		if platform == "linux":
+			AptInstall().installPackages('libcairo2-dev', 'libfreetype6-dev', 'libffi-dev', 'libjpeg-dev', 'libpng-dev', 'libz-dev', accept=True)
