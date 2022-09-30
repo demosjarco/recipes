d3ca91ed4f351853324310ba6915572ad06d063d
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2022-09-30 15:32:33 -0700

    Rename parameter

--- hooks/preSetup.py
+++ hooks/preSetup.py
@@ -11,11 +11,11 @@ class AptInstall:
 		else:
 			return True
 
-	def installPackages(self, *packages:str, accept:bool = False) -> None:
-		system(f'{self.isSudo() == True and "" or "sudo"} apt install {accept == True and "-y" or ""} {" ".join(packages)}')
+	def installPackages(self, *packages:str, assumeYes:bool = False) -> None:
+		aptAttempt = system(f'{self.isSudo() == True and "" or "sudo"} apt install {assumeYes == True and "-y" or ""} {" ".join(packages)}')
 
 def on_startup(command, dirty:bool):
 	if (getenv('ENABLED_SOCIAL') != None and bool(getenv('ENABLED_SOCIAL'))):
 		# MkDocs social requirement
 		if platform == "linux":
-			AptInstall().installPackages('libcairo2-dev', 'libfreetype6-dev', 'libffi-dev', 'libjpeg-dev', 'libpng-dev', 'libz-dev', accept=True)
+			AptInstall().installPackages('libcairo2-dev', 'libfreetype6-dev', 'libffi-dev', 'libjpeg-dev', 'libpng-dev', 'libz-dev', assumeYes=True)
