15c03d407bf273b48ba2756f1263ec516579111c
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2022-09-30 12:41:57 -0700

    No longer used

--- setupScripts/docs.py
+++ setupScripts/docs.py
@@ -1,9 +1,5 @@
-import os
-import subprocess
-from .ciSystem import CiSystem
 from .cookLang import CookLang
 from .cookDocs import CookDocs
-import shutil
 
 class Docs:
 	def __init__(self) -> None:
