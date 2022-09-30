d0039e7ec58dd837daace12ca5d90b75fbeac1f3
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2022-09-30 12:41:20 -0700

    Move to hooks

--- setupScripts/docs.py
+++ setupScripts/docs.py
@@ -7,12 +7,5 @@ import shutil
 
 class Docs:
 	def __init__(self) -> None:
-		# self.systemDebug()
 		CookLang()
 		CookDocs()
-	
-	def systemDebug(self) -> None:
-		print('Detected CI system', self.systemType, flush=True)
-		print('CI_SYSTEM_OVERRIDE', os.getenv('CI_SYSTEM_OVERRIDE'), flush=True)
-		print('CF_PAGES', os.getenv('CF_PAGES'), flush=True)
-		print('GITHUB_ACTIONS', os.getenv('GITHUB_ACTIONS'), flush=True)
