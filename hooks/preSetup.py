7b9173e63b4d907c63a4f48addf91988970c2350
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2022-09-30 15:40:22 -0700

    Better debug

--- hooks/preSetup.py
+++ hooks/preSetup.py
@@ -13,7 +13,7 @@ class AptInstall:
 
 	def installPackages(self, *packages:str, assumeYes:bool = False) -> None:
 		aptAttempt = system(f'{self.isSudo() == True and "" or "sudo"} apt install {assumeYes == True and "-y" or ""} {" ".join(packages)}')
-		print("Exit", WEXITSTATUS(aptAttempt))
+		print("EXIT CODE:", WEXITSTATUS(aptAttempt))
 
 def on_startup(command, dirty:bool):
 	if (getenv('ENABLED_SOCIAL') != None and bool(getenv('ENABLED_SOCIAL'))):
